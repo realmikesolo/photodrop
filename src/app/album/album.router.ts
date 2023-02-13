af4db4229430c072357894200cd5bd830e6413ba
realmikesolo <aeryoff@gmail.com>
2023-02-13 15:57:29 +0100

    add endpoint albumsByUser

--- src/app/album/album.router.ts
+++ src/app/album/album.router.ts
@@ -17,11 +17,24 @@ export async function albumRouter(fastify: FastifyInstance): Promise<void> {
     '/album/upload',
     {
       schema: AlbumCreateAlbumSchema,
-      preHandler: [verifyJWT],
-    },
+      preHandler: [verifyJWT],    },
     async (req, res) => {
       const { status, body } = await albumService.createAlbum({
         ...req.body,
+        photographerId: (req as AuthRequest).photographerId,
+      });
+      return res.status(status).send(body);
+    },
+  );
+
+  fastify.get(
+    '/album/allByUser',
+    {
+      schema: AlbumGetAlbumsSchema,
+      preHandler: [verifyJWT],
+    },
+    async (req, res) => {
+      const { status, body } = await albumService.getAlbums({
         photographerId: (req as AuthRequest).photographerId,
       });
       return res.status(status).send(body);
@@ -38,6 +51,27 @@ export const AlbumCreateAlbumSchema = {
   },
 };
 
+export const AlbumGetAlbumsSchema = {
+  response: {
+    200: responseSchema({
+      albums: Type.Optional(
+        Type.Array(
+          Type.Object({
+            id: Type.Number(),
+            name: Type.String(),
+            location: Type.String(),
+            date: Type.String(),
+          }),
+        ),
+      ),
+    }),
+  },
+};
+
 export type AlbumCreateAlbumContext = Static<typeof AlbumCreateAlbumSchema['body']> & {
+  photographerId: number;
+};
+
+export type AlbumGetAlbumsContext = {
   photographerId: number;
 };
--- src/app/album/album.service.ts
+++ src/app/album/album.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException } from '../../http/exception';
 import { HttpStatus } from '../../http/status';
 import { HttpResponse } from '../../http/type';
 import { Album } from './album.entity';
-import { AlbumCreateAlbumContext } from './album.router';
+import { AlbumCreateAlbumContext, AlbumGetAlbumsContext } from './album.router';
 
 export class AlbumService {
   public async createAlbum(ctx: AlbumCreateAlbumContext): Promise<HttpResponse> {
@@ -23,6 +23,22 @@ export class AlbumService {
       status: HttpStatus.OK,
       body: {
         id: album.id,
+        success: true,
+      },
+    };
+  }
+
+  public async getAlbums(ctx: AlbumGetAlbumsContext): Promise<HttpResponse> {
+    const albums = await Album.find({
+      where: {
+        photographer: { id: ctx.photographerId },
+      },
+    });
+
+    return {
+      status: HttpStatus.OK,
+      body: {
+        albums,
         success: true,
       },
     };
